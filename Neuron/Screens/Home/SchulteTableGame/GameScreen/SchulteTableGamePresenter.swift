8b185d81285816225cc1a8b35a823f7d22b9e106
Anar <anardadashov00@gmail.com>
2019-11-21 23:33:57 +0300

    perf(schulte_table_game): подготовительные работы для написания кода
    Связал все элементы с кодом, выстроил MVP, наладил связи между классами через протоколы.

---
+++ Neuron/Screens/Home/SchulteTableGame/GameScreen/SchulteTableGamePresenter.swift
@@ -0,0 +1,25 @@
+//
+//  SchulteTableGamePresenter.swift
+//  Neuron
+//
+//  Created by Anar on 21.11.2019.
+//  Copyright © 2019 Commodo. All rights reserved.
+//
+
+import Foundation
+
+protocol SchulteTableGamePresenterDelegate {
+  func viewDidload()
+}
+
+final class SchulteTableGamePresenter: SchulteTableGamePresenterDelegate {
+  var view: SchulteTableGameViewControllerDelegate?
+  
+  init(view: SchulteTableGameViewControllerDelegate) {
+    self.view = view
+  }
+  
+  func viewDidload() {
+    
+  }
+}
