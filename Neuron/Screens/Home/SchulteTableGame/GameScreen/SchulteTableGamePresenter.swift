9b6ec81b5d54769610bac8c20f0d0213e3f8ffc2
Anar <anardadashov00@gmail.com>
2019-11-30 21:00:20 +0300

    perf(schulte_table_game): реализовано UI ячеек с смешиванием оттенков
    В метод getColorsArray добавлена необходимая логика. Вначале выбирается один из основных цветов (синий, зеленый, красный), затем получаем тон соответсвующего цвета из словаря и если тон самый светлый, то цвет текста лэйбла делаем темным, а для остальных тонов он светлый.

--- Neuron/Screens/Home/SchulteTableGame/GameScreen/SchulteTableGamePresenter.swift
+++ Neuron/Screens/Home/SchulteTableGame/GameScreen/SchulteTableGamePresenter.swift
@@ -35,10 +35,10 @@ final class SchulteTableGamePresenter: SchulteTableGamePresenterDelegate {
   // MARK: - configureGame
   func configureGame() {
     let smashedCellsCount = self.getSmashedCellsCount()
-    let colorsDict        = self.getColorsArray(smashedCellsCount)
+    let colorsArray       = self.getColorsArray(smashedCellsCount)
     let cellsNumbers      = self.getCellsNumbers()
     
-    self.view!.tableCollectionViewCellsData = self.getTableCollectionViewCellsData(cellsNumbers, colorsDict.map{ $0.0 }, colorsDict.map{ $0.1 })
+    self.view!.tableCollectionViewCellsData = self.getTableCollectionViewCellsData(cellsNumbers, colorsArray.map{ $0.0 }, colorsArray.map{ $0.1 })
   }
   
   // MARK: - getSmashedCellsCount
@@ -63,11 +63,62 @@ final class SchulteTableGamePresenter: SchulteTableGamePresenterDelegate {
     let colors = [UIColor(red: 0.459, green: 0.608, blue: 0.98, alpha: 1),  // Blue
                   UIColor(red: 0.315, green: 0.788, blue: 0.22, alpha: 1),  // Green
                   UIColor(red: 0.992, green: 0.314, blue: 0.314, alpha: 1)] // Red
-    var colorsArray = [(UIColor, UIColor)]() // [(backgroundColor: labelTextColor)]
+    
+    let blueShades = ["mostLight": UIColor(red: 0.784, green: 0.839, blue: 0.967, alpha: 1),
+                      "light": UIColor(red: 0.176, green: 0.612, blue: 0.859, alpha: 1),
+                      "blue": UIColor(red: 0.459, green: 0.608, blue: 0.98, alpha: 1),
+                      "dark": UIColor(red: 0.358, green: 0.467, blue: 0.739, alpha: 1)]
+    
+    let greenShades = ["mostLight": UIColor(red: 0.788, green: 0.93, blue: 0.765, alpha: 1),
+                       "light": UIColor(red: 0.435, green: 0.812, blue: 0.592, alpha: 1),
+                       "green": UIColor(red: 0.315, green: 0.788, blue: 0.22, alpha: 1),
+                       "dark": UIColor(red: 0.129, green: 0.588, blue: 0.325, alpha: 1)]
+    
+    let redShades = ["mostLight": UIColor(red: 0.979, green: 0.82, blue: 0.82, alpha: 1),
+                     "light": UIColor(red: 1, green: 0.451, blue: 0.451, alpha: 1),
+                     "red": UIColor(red: 0.992, green: 0.314, blue: 0.314, alpha: 1),
+                     "dark": UIColor(red: 0.801, green: 0.265, blue: 0.265, alpha: 1)]
+    
+    var colorsArray = [(UIColor, UIColor)]() // [(backgroundColor, labelTextColor)]
     
     for (index, count) in smashedCellsCount.enumerated() {
       for _ in 0..<count {
-        colorsArray.append((colors[index], UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1)))
+        switch self.view!.settingsData.mixingShades {
+        case true:
+            switch colors[index] {
+            case UIColor(red: 0.459, green: 0.608, blue: 0.98, alpha: 1): // Blue
+                if let blueShade = blueShades.randomElement() {
+                    switch blueShade.value {
+                    case blueShades["mostLight"]:
+                        colorsArray.append((blueShade.value, UIColor(red: 0.153, green: 0.239, blue: 0.322, alpha: 1)))
+                    default:
+                        colorsArray.append((blueShade.value, UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1)))
+                    }
+                }
+            case UIColor(red: 0.315, green: 0.788, blue: 0.22, alpha: 1): // Green
+                if let greenShade = greenShades.randomElement() {
+                    switch greenShade.value {
+                    case greenShades["mostLight"]:
+                        colorsArray.append((greenShade.value, UIColor(red: 0.153, green: 0.239, blue: 0.322, alpha: 1)))
+                    default:
+                        colorsArray.append((greenShade.value, UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1)))
+                    }
+                }
+            case UIColor(red: 0.992, green: 0.314, blue: 0.314, alpha: 1): // Red
+                if let redShade = redShades.randomElement() {
+                    switch redShade.value {
+                    case redShades["mostLight"]:
+                        colorsArray.append((redShade.value, UIColor(red: 0.153, green: 0.239, blue: 0.322, alpha: 1)))
+                    default:
+                        colorsArray.append((redShade.value, UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1)))
+                    }
+                }
+            default:
+                fatalError("How did i get here?")
+            }
+        case false:
+            colorsArray.append((colors[index], UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1)))
+        }
       }
     }
     
